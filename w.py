f7d45ef55b4ab330d3dc8a21b190a30c333629c7
werest <weresttrade@ya.ru>
2020-01-27 01:11:14 +0300

    fix - подготовка по всем точкам скопления (cluster points)

--- w.py
+++ w.py
@@ -14,15 +14,19 @@ def J(x, xc, y, yc, brightness):
 
 
 # условная функция рассчёта евклидово расстояния
-def calc(image):
+# cluster_of_points - скопление точек
+def calc(image, index_cluster_of_points):
     array_J = np.zeros((image.shape[0], image.shape[1]))
 
     for i in range(0, image.shape[0]):
         for j in range(0, image.shape[1]):
+            for r in range(0, index_cluster_of_points.shape[0]):
+                for u in range(0, index_cluster_of_points.shape[1]):
+
             X_c = i
             Y_c = j
 
-            X_1 = 140
+            X_1 = 150
             Y_1 = 114
 
             brightness_0 = image[X_1, Y_1]
@@ -30,8 +34,10 @@ def calc(image):
             array_J[i, j] = J(X_1, X_c, Y_1, Y_c, brightness_0)
 
     m_J = np.amin(array_J)
-    print(np.where(array_J == m_J))
     pd.DataFrame(array_J).to_csv('121.csv', sep=',')
+
+    print(np.where(array_J == m_J))
+    print(m_J)
 
     pass
 
@@ -41,7 +47,7 @@ def c1(image, number):
     fig, (ax, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(8, 3),
                                   sharex=True, sharey=True)
     ax.axis('on')
-    ax2.axis('off')
+    ax2.axis('on')
 
     ax.imshow(image)
 
@@ -50,7 +56,7 @@ def c1(image, number):
     # Y, X
     ax2.imshow(image)
     plt.savefig('k/{}'.format(number))
-    calc(image)
+    calc(image, ind)
 
 
 path_img = 'konstantin/2019.10.21 ФИ-65/2019.10.21_actReg/A7 76_ac.png'
