724f1bb917f4ff91dd00012c631cf52eab3016eb
werest <weresttrade@ya.ru>
2020-02-04 20:26:50 +0300

    Разработка под множество ценроидов - 1

--- ui2.py
+++ ui2.py
@@ -18,15 +18,6 @@ def Ex(x, xc, y, yc, label):
 
 
 def search_countrs(image, level):
-    counts = measure.find_contours(image, level)
-
-    Xc = np.random.randint(50, 100, len(counts))
-    Yc = np.random.randint(50, 100, len(counts))
-    c1(image, Xc, Yc)
-
-
-# Xc, Yc - координаты центроида
-def c1(image, Xc, Yc):
     fig, (ax, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(8, 3),
                                        sharex=True, sharey=True)
     ax.axis('on')
@@ -35,15 +26,35 @@ def c1(image, Xc, Yc):
     ax.imshow(image)
     ax3.imshow(image)
 
+    counts = measure.find_contours(image, level)
+
+    Xc = np.random.randint(50, 100, len(counts))
+    Yc = np.random.randint(50, 100, len(counts))
+    X, Y = 0.0, 0.0
+    for i in range(len(Xc)):
+        X, Y = c1(image, Xc[i], Yc[i])
+        ax2.plot(X, Y, marker='x', markersize='10')
+        print('X0', X, '//', 'Y0', Y)
+
+
+    ax2.imshow(image)
+
+    # plt.plot(X0, Y0, marker='x', markersize='10')
+    plt.savefig('k/50')
+
+# Xc, Yc - координаты центроида
+def c1(image, Xc, Yc):
     ind = np.where(image >= 0.9)
     step = 0.01
 
     X = ind[0]
     Y = ind[1]
 
+    X0 = Xc
+    Y0 = Yc
+
     d = True
     flag = 0
-    iii = 0
     while d:
         sumEx = 0.0
         xxx = X0
@@ -51,39 +62,24 @@ def c1(image, Xc, Yc):
         if flag == 0:
             for i in range(0, len(X)):
                 sumEx = sumEx + Ex(X[i], X0, Y[i], Y0, label=0)
-            print(sumEx)
             X0 = X0 + step * sumEx
             flag = 1
             if int(X0) == int(xxx):
                 d = False
-            print('X0', X0, '//', xxx)
         else:
             for i in range(0, len(X)):
                 sumEx = sumEx + Ex(X[i], X0, Y[i], Y0, label=1)
             Y0 = Y0 + step * sumEx
-            print(sumEx)
             flag = 0
             if int(Y0) == int(yyy):
                 d = False
-            print('Y0', Y0, '//', yyy)
-        iii = iii + 1
+    return X0, Y0
 
-    ax2.imshow(image)
-    ax2.plot(X0, Y0, marker='x', markersize='10')
-
-    print('Итераций прошло:', iii)
-    plt.plot(X0, Y0, marker='x', markersize='10')
-    plt.savefig('k/50')
-
-
-path_img = 'konstantin/2019.10.02 ФИ-59/2019.10.02_actReg/2019.10.02_3/B3 97_ac.png'
-path_img_v = 'konstantin/2019.10.02 ФИ-59/2019.10.02_actReg/2019.10.02_5/B5 97_an.png'
+path_img = 'konstantin/2019.10.02 ФИ-59/2019.10.02_actReg/2019.10.02_7/B7 97_ac.png'
 
 image = color.rgb2gray(io.imread(path_img))
-image_v = color.rgb2gray(io.imread(path_img_v))
 image = cv2.blur(image, (3, 3))
 x0 = 10
 y0 = 10
 
-print(image.shape)
 search_countrs(image, 0.8)
