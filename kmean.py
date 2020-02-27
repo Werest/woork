f2e652ff7255928677c47f642428257bf5d0fc59
werest <weresttrade@ya.ru>
2020-02-27 13:12:15 +0300

    fix

--- kmean.py
+++ kmean.py
@@ -9,40 +9,39 @@ import numpy as np
 import os
 
 
-def kmeans(image, level, num):
+def kmeans(imagep, level_, num):
     fig, (ax, ax1) = plt.subplots(nrows=1, ncols=2, figsize=(8, 3))
     ax.axis('on')
 
-    ax.imshow(image)
-    ax1.imshow(image)
+    ax.imshow(imagep)
+    ax1.imshow(imagep)
 
-    counts = measure.find_contours(image, level)
-    for n, contour in enumerate(counts):
-        ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
+    ci = measure.find_contours(imagep, level_)
 
     yc = []
     xc = []
     bt = []
-    for n, contour in enumerate(counts):
+    for n, contour in enumerate(ci):
         for c in contour:
             yc.append(int(c[1]))
             xc.append(int(c[0]))
-            bt.append(image[int(c[0]), int(c[1])])
+            bt.append(imagep[int(c[0]), int(c[1])])
 
-    print(len(xc), "--", len(yc))
-    z = [list(hhh) for hhh in zip(xc, yc, 20 * bt)]
-    k = KMeans(n_clusters=len(counts), random_state=0, n_init=100).fit(z)
-    ax.plot(k.cluster_centers_[:, 1], k.cluster_centers_[:, 0], marker='+', markersize='5')
-    print("Центроиды: \n", k.cluster_centers_)
+    if len(xc) > 0 and len(yc) > 0:
+        z = [list(hhh) for hhh in zip(xc, yc, bt)]
+        k = KMeans(n_clusters=len(ci), n_init=20).fit(z)
+        ax.plot(k.cluster_centers_[:, 1], k.cluster_centers_[:, 0], marker='+', markersize='5')
+        print("Центроиды: \n", k.cluster_centers_)
+        plt.savefig('k/{}'.format(num))
+    else:
+        print("Не можем определить центроды")
 
-    plt.savefig('k/{}'.format(num))
 
-
-path_img = "konstantin/2019.12.20 ФИ-80/2019.12.20_actReg/2019.12.20_3/A3 111_ac.png"
+path_img = "konstantin/2019.12.20 ФИ-80/2019.12.20_actReg/2019.12.20_16/A16 111_ac.png"
 
 image = color.rgb2gray(io.imread(path_img))
 image = cv2.blur(image, (5, 5))
-kmeans(image, level=0.2, num=558)
+kmeans(image, level_=0.8, num=559)
 
 # files = os.listdir('Attachments_lalv@yandex')
 # for num, ftf in enumerate(files):
