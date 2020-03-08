2f64928a0b4be5629c7e6416a1177a48abcba818
werest <weresttrade@ya.ru>
2020-03-08 08:40:34 +0300

    Работа с БД

--- kmean.py
+++ kmean.py
@@ -5,6 +5,7 @@ from skimage import measure
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import os
+import pymysql
 
 
 def kmeans(imagep, level_, number):
@@ -34,19 +35,49 @@ def kmeans(imagep, level_, number):
         x_t = list(k.cluster_centers_[:, 0])
         y_t = list(k.cluster_centers_[:, 1])
         ax.scatter(y_t, x_t, s=5, c='red')
-        print("Центроиды: \n", k. cluster_centers_)
+        print("Центроиды: \n", k.cluster_centers_)
         plt.savefig('k/{}'.format(number))
         plt.close(fig)
     else:
         print("Не можем определить центроиды")
 
 
-files = os.listdir('ko2')
-for num, ftf in enumerate(files):
-    plt.cla()
-    plt.clf()
-    print(ftf)
-    opa = 'ko2/' + ftf
-    image = color.rgb2gray(io.imread(opa))
-    image = cv2.blur(image, (3, 3))
-    kmeans(image, level_=(image.max() - 0.1), number=num)
+connection = pymysql.connect(host='localhost',
+                             user='newuser',
+                             password='PASSWORD',
+                             db='dbo',
+                             charset='utf8',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+
+def sql():
+    try:
+        with connection.cursor() as cursor:
+            sql = "call dbo.get_File();"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            for j, res in enumerate(result):
+                file = 'konstantin' + result[j]['name_file']
+                if os.path.exists(file):
+                    sp = file.split('/')
+                    for i, k in enumerate(sp[:-1]):
+                        sp[i] = sp[i] + '_'
+                    name_finally = ''.join(sp[1:])
+                    image = color.rgb2gray(io.imread(file))
+                    image = cv2.blur(image, (3, 3))
+                    kmeans(image, level_=(image.max() - 0.1), number=name_finally)
+    finally:
+        connection.close()
+
+
+sql()
+
+# files = os.listdir('ko2')
+# print(files)
+# for num, ftf in enumerate(files):
+#     plt.cla()
+#     plt.clf()
+#     opa = 'ko2/' + ftf
+#     image = color.rgb2gray(io.imread(opa))
+#     image = cv2.blur(image, (3, 3))
+#     kmeans(image, level_=(image.max() - 0.1), number=num)
