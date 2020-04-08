b128c64e06626d771924058fb3eabb4c14ec0aff
werest <weresttrade@ya.ru>
2020-04-08 09:57:13 +0300

    logging, zip, video

--- kmean.py
+++ kmean.py
@@ -1,3 +1,4 @@
+import cv2
 from skimage import color, measure
 from skimage import io
 from sklearn.cluster import KMeans, MeanShift
@@ -6,9 +7,16 @@ import os
 import pymysql
 import numpy as np
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, normalize
+import logging
+from zipfile import ZipFile, ZIP_DEFLATED
+
+# config for logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+                    , level=logging.INFO)
+log = logging.getLogger(__name__)
 
 
-def km(img, number, g):
+def km(img, number, g, outdir):
     plt.cla()
     plt.clf()
 
@@ -53,26 +61,44 @@ def km(img, number, g):
         x_t = list(k.cluster_centers_[:, 0])
         y_t = list(k.cluster_centers_[:, 1])
         ax.scatter(y_t, x_t, s=5, c='red')
-        print("Центроиды: \n", k.cluster_centers_)
-        plt.savefig('k1/{}'.format(number))
+        # print("Центроиды: \n", k.cluster_centers_)
+        # logging.info()
+        plt.savefig('{}/{}'.format(outdir, number))
 
         plt.close(fig)
     else:
         print("Не можем определить центроиды")
 
 
-connection = pymysql.connect(host='localhost',
-                             user='newuser',
-                             password='PASSWORD',
-                             db='dbo',
-                             charset='utf8',
-                             cursorclass=pymysql.cursors.DictCursor)
+def gen_video():
+    img_folder = 'k1'
+    video_name = '1.avi'
+    fileid = 'video.zip'
+
+    if not os.path.isfile(video_name):
+        imgs = [img for img in os.listdir(img_folder)]
+        frame = cv2.imread(os.path.join(img_folder, imgs[0]))
+        height, width, layers = frame.shape
+        video = cv2.VideoWriter(video_name, 0, 1, (width, height))
+
+        for image in imgs:
+            video.write(cv2.imread(os.path.join(img_folder, image)))
+        cv2.destroyAllWindows()
+        video.release()
+
+    if not os.path.isfile(fileid):
+        log.info('Создание zip файла - %s', fileid)
+        with ZipFile(fileid, mode='w', compression=ZIP_DEFLATED) as misfile:
+            misfile.write(video_name)
 
 
-def f_dir(d, p):
-    remove_ds_store = [name for name in os.listdir(d) if not name.startswith('.')]
+def f_dir(d, p, od):
+    log.info('Сканирование директории для исследования')
+    remove_ds_store = [name for name in os.listdir(d) if not name.startswith(('.', 'ORG'))]
     sort_list = sorted(remove_ds_store)
+    log.info('Найдено %s образца', len(sort_list))
 
+    log.info('Поиск центроидов начат')
     for num, path in enumerate(sort_list):
         # ЧБ
         path = d + path
@@ -85,12 +111,22 @@ def f_dir(d, p):
         image = np.where(raze, 0, image)
         gosh = np.where(image >= fast)
 
-        km(image, number=num, g=gosh)
+        km(image, number=num, g=gosh, outdir=od)
         # plt.scatter(gosh[0], gosh[1], color='red')
         # plt.show()
-        if num == 0:
-            break
+        # if num == 0:
+        #     break
+    log.info('Поиск центроидов окончен')
 
 
+# config directory, files and etc.
 directory = "2020-2/A4 98 um 20200325/"
-f_dir(directory, 0.2)
+output_dir = 'k1'
+
+log.info('Директория для исследования - %s', directory)
+log.info('Директория для выходных изображений - %s', output_dir)
+f_dir(d=directory, p=0.2, od=output_dir)
+
+log.info('Началась генерация видео')
+gen_video()
+log.info('Окончена генерация видео')
