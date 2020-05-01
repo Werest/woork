37c59d525c4f062b47e6c85436ed1a1c1562e2b6
werest <weresttrade@ya.ru>
2020-05-01 14:10:15 +0300

    Размер + вернуть контур

--- cus_de.py
+++ cus_de.py
@@ -16,10 +16,6 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     , level=logging.INFO)
 log = logging.getLogger(__name__)
 
-array_x_t = []
-array_y_t = []
-
-
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
@@ -32,7 +28,6 @@ class Ui(QtWidgets.QMainWindow):
         self.input_2 = self.findChild(QtWidgets.QDoubleSpinBox, 'doubleSpinBox_2')
         # Размер
         self.input_x = self.findChild(QtWidgets.QDoubleSpinBox, 'doubleSpinBox')
-
         # label
         self.label_max = self.findChild(QtWidgets.QLabel, 'label_3')
         self.label_min = self.findChild(QtWidgets.QLabel, 'label_4')
@@ -84,23 +79,23 @@ class Ui(QtWidgets.QMainWindow):
         self.axes.scatter(y_t, x_t, s=5, c='red')
         # длина вектора по координатам
         # AB = sqrt (bx - ax)^2 + (by-ay)^2
-        DD_vector = []
-        for n, contour in enumerate(contours):
-            A_Xmin = min(contour[:, 0])
-            A_Ymax = max(contour[:, 1])
-
-            B_Xmax = max(contour[:, 0])
-            B_Ymin = min(contour[:, 1])
-            D_vector = pow((B_Xmax - A_Xmin), 2) + pow((B_Ymin - A_Ymax), 2)
-            D_vector = math.sqrt(D_vector) * caff
-            DD_vector.append(D_vector)
-            if D_vector >= rz_x:
-                self.axes2.plot(contour[:, 0], contour[:, 1], linewidth=2)
+        # DD_vector = []
+        # for n, contour in enumerate(contours):
+        #     A_Xmin = min(contour[:, 0])
+        #     A_Ymax = max(contour[:, 1])
+        #
+        #     B_Xmax = max(contour[:, 0])
+        #     B_Ymin = min(contour[:, 1])
+        #     D_vector = pow((B_Xmax - A_Xmin), 2) + pow((B_Ymin - A_Ymax), 2)
+        #     D_vector = math.sqrt(D_vector) * caff
+        #     DD_vector.append(D_vector)
+        #     if D_vector >= rz_x:
+        #         self.axes2.plot(contour[:, 0], contour[:, 1], linewidth=2)
 
         self.plot.draw_idle()
-        max_DD_v, min_DD_v = max(DD_vector), min(DD_vector)
-        self.label_max.setText('Максимальная гломерула - {} мкм'.format(str(max_DD_v)))
-        self.label_min.setText('Минимальная гломерула - {} мкм'.format(str(min_DD_v)))
+        # max_DD_v, min_DD_v = max(DD_vector), min(DD_vector)
+        # self.label_max.setText('Максимальная гломерула - {} мкм'.format(str(max_DD_v)))
+        # self.label_min.setText('Минимальная гломерула - {} мкм'.format(str(min_DD_v)))
 
     def export_csv(self):
         rz_x = float(self.input_x.text().replace(',', '.'))
@@ -132,6 +127,8 @@ class Ui(QtWidgets.QMainWindow):
         y = g[1]
         # Если имеется массив центроидов
         if len(x) > 0 and len(y) > 0:
+            x_t = []
+            y_t = []
             mkm_width, caff = self.rz(1214.6, img, rz_x)
 
             # zip (..., ..., img[x, y])
@@ -145,11 +142,43 @@ class Ui(QtWidgets.QMainWindow):
             contours = measure.find_contours(img, 0.5)
 
             k = KMeans(n_clusters=vis.elbow_value_).fit(z)
-            x_t = list(k.cluster_centers_[:, 0])
-            y_t = list(k.cluster_centers_[:, 1])
 
-            array_x_t.append(x_t)
-            array_y_t.append(y_t)
+            # [[z[i] for i in range(len(k.labels_))]]
+            # arrayp = [for j in range(len() if j == k.labels_ [z[i] for i in range(len(k.labels_))]]
+            arrayp = [[0]]* len(k.cluster_centers_)
+            for d, c in enumerate(arrayp):
+                kkk = []
+                for i, m in enumerate(k.labels_):
+                    if m == d:
+                        kkk.append(z[i])
+                arrayp[d] = np.array(kkk)
+
+            hhhhh = len(k.cluster_centers_)
+            DD_vector = []
+            for n, aaa in enumerate(arrayp):
+                A_Xmin = min(aaa[:, 0])
+                A_Ymax = max(aaa[:, 1])
+
+                B_Xmax = max(aaa[:, 0])
+                B_Ymin = min(aaa[:, 1])
+                D_vector = pow((B_Xmax - A_Xmin), 2) + pow((B_Ymin - A_Ymax), 2)
+                D_vector = math.sqrt(D_vector) * caff
+                DD_vector.append(D_vector)
+                if D_vector <= rz_x:
+                    g = aaa[:].tolist()
+                    z = [s for s in z if s not in g]
+                    hhhhh = hhhhh - 1
+
+            print(DD_vector)
+            if len(z) > 0:
+                k = KMeans(n_clusters=hhhhh).fit(z)
+
+                x_t = list(k.cluster_centers_[:, 0])
+                y_t = list(k.cluster_centers_[:, 1])
+            else:
+                self.label_max.setText('Заданный размер слишком высок')
+
+
             log.info('Параметр порога - {}'.format(parametr_p))
 
             return img, contours, y_t, x_t, parametr_p, mkm_width, caff, k.cluster_centers_
@@ -160,9 +189,7 @@ class Ui(QtWidgets.QMainWindow):
         iw, ih = img.shape[0], img.shape[1]
         # поиск сколько приходится на 1 пиксель мкм
         caff = mkm / iw
-
         mkm_width = round(caff * rz_x)
-
         return mkm_width, caff
 
     def f_dir(self, d, p, od, vn, fd, rz_x):
